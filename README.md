fc5d3e287f9b364f8a76ab02bc57c62eea252850
rmamiyev0 <1155135439@link.cuhk.edu.hk>
2022-03-05 20:59:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# strange-planet-mini-game
---
+++ python-skeleton/Cell.py
@@ -0,0 +1,92 @@
+"""
+CSCI3180 Principles of Programming Languages
+
+--- Declaration ---
+
+I declare that the assignment here submitted is original except for source
+material explicitly acknowledged. I also acknowledge that I am aware of
+University policy and regulations on honesty in academic work, and of the
+disciplinary guidelines and procedures applicable to breaches of such policy
+and regulations, as contained in the website
+http://www.cuhk.edu.hk/policy/academichonesty/
+
+Assignment 2
+Name : Resul Mamiyev
+Student ID : 1155135439
+Email Addr : rmamiyev0@cse.cuhk.edu.hk
+"""
+
+from abc import abstractmethod
+
+
+class Cell:
+    def __init__(self, row=0, col=0):
+        self._row = row
+        self._col = col
+        self._occupant = None
+        self._color = None
+        self._hours = 0
+
+    # TODO: hours getter and setter
+
+    @property
+    def hours(self):
+        return self._hours
+
+    @hours.setter
+    def hours(self, a):
+        self._hours = a
+
+    @property
+    def occupant(self):
+        return self._occupant
+
+    def set_occupant(self, obj):
+        # TODO: set occupant for the Plain cell
+        #       return whether success or not
+        if(self._occupant == None or self._occupant.interact_with(obj)):
+            self._occupant = obj
+            return True
+        else:
+            return False
+        # END TODO
+
+    def remove_occupant(self):
+        # TODO: remove the occupant
+        self._occupant = None
+        # END TODO
+
+    def display(self):
+        # TODO: print a string to display the cell
+        #       and the occupant in the cell
+        if (self._occupant != None):
+            print(self._color, self._occupant.display(),
+                  self._color, end='\033[0m   ')
+        else:
+            print(self._color, end='   \033[0m   ')
+        # END TODO
+
+
+class Plain(Cell):
+    def __init__(self, row, col):
+        Cell.__init__(self, row, col)
+        self._color = '\033[1;32;42m'
+        self._hours = 1
+
+
+class Mountain(Cell):
+    def __init__(self, row, col):
+        Cell.__init__(self, row, col)
+        self._color = '\033[1;37;47m'
+
+    def set_occupant(self, obj):
+        # TODO: return False
+        return False
+        # END TODO
+
+
+class Swamp(Cell):
+    def __init__(self, row, col):
+        Cell.__init__(self, row, col)
+        self._color = '\033[1;34;44m'
+        self._hours = 2
